bea2511b705a34f9c8d5e826c04cb32af3fb2435
Andrea Lin <me@andrealin.it>
2025-03-01 22:32:51 +0100

    feat: add svg optimization to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import { run } from 'vite-plugin-run'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    run([
+      {
+        name: "Optimize icons SVGs",
+        run: ["npm", "run", "icons:optimize"],
+        pattern: [
+          "assets/icons/*.svg",
+          "config/svgo.config.cjs"
+        ]
+      }
+    ]),
+  ],
 })
