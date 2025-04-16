0ec57222ca016368b9c7ea77fb02a3a6e05a7e59
Andrea Lin <me@andrealin.it>
2025-04-16 23:34:01 +0200

    fix: `EMFILE` too many open files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { run } from 'vite-plugin-run'
 import { iconsSpritesheet } from "vite-plugin-icons-spritesheet"
 
 // https://vite.dev/config/
@@ -9,16 +8,6 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    run([
-      {
-        name: "Optimize icons SVGs",
-        run: ["npm", "run", "icons:optimize"],
-        pattern: [
-          "assets/icons/*.svg",
-          "config/svgo.config.cjs"
-        ]
-      }
-    ]),
     iconsSpritesheet({
       inputDir: "assets/icons/optimized",
       outputDir: "src/components/icon",
@@ -31,16 +20,6 @@ export default defineConfig({
           .map((part) => part.toLowerCase()).join("-")
       }
     }),
-    run([
-      {
-        name: "Optimize colored icons SVGs",
-        run: ["npm", "run", "colored:optimize"],
-        pattern: [
-          "assets/colored/*.svg",
-          "config/colored-svgo.config.cjs"
-        ]
-      }
-    ]),
     iconsSpritesheet({
       inputDir: "assets/colored/optimized",
       outputDir: "src/components/colored-icon",
@@ -51,16 +30,6 @@ export default defineConfig({
         return name.normalize().toLowerCase()
       }
     }),
-    run([
-      {
-        name: "Optimize duotone icons SVGs",
-        run: ["npm", "run", "duotone:optimize"],
-        pattern: [
-          "assets/duotone/*.svg",
-          "config/svgo.config.cjs"
-        ]
-      }
-    ]),
     iconsSpritesheet({
       inputDir: "assets/duotone/optimized",
       outputDir: "src/components/duotone-icon",
